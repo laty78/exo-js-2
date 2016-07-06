6140b9128d055e2cff5bd8a4027482f971cb3741
boumizy <boumizylatifa@gmail.com>
2016-07-06 17:00:54 +0200

    doc exo

--- questions.js
+++ questions.js
@@ -7,3 +7,26 @@
 /**
  * Exercices sur les nombres et les caluls mathématiques
  */
+function tailleString(texte){
+	return texte.length;
+}
+function remplaceCar(texte,avant,apres){
+ 	return texte.replace(avant,apres);
+
+}
+function concatString(texte1, texte2){
+	return texte1 + texte2;
+}
+
+function afficherCar(texte){
+	 return texte.charAt(4); 
+}
+function afficherNCar(texte){
+	return texte.substring(0,9);
+}
+function majusculeString(texte){
+	return texte.toUpperCase();
+}
+function SupprEspaceString(texte){
+	return texte.trim();
+}
